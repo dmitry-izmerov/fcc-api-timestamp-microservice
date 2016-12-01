bafc50ba1ee9c5ff945f420ff21cecc4bf01037f
dmitry <idd90y@yandex.ru>
2016-12-01 20:54:25 +0000

    added ability to set port and ip from env

--- server.js
+++ server.js
@@ -31,9 +31,7 @@ app.get('/:time', function (req, res) {
   res.send(response);
 });
 
-
-// process.env.PORT || 3000, process.env.IP || "0.0.0.0"
-let server = app.listen(8080, "0.0.0.0", function () {
+let server = app.listen(process.env.PORT || 8080, process.env.IP || "0.0.0.0", function () {
   let addr = server.address();
   console.log("Server is listening at", addr.address + ":" + addr.port);
 });
